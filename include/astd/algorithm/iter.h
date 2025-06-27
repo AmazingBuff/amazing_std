736a6c58d985ae3c1d0aa9c5f74922183c660767
AmazingBuff <lhq499445413@163.com>
2025-06-27 21:56:50 +0800

    [astd]: adjust tree and add some iterator algorithm

---
+++ include/astd/algorithm/iter.h
@@ -0,0 +1,61 @@
+//
+// Created by AmazingBuff on 2025/6/26.
+//
+
+#ifndef ITER_H
+#define ITER_H
+
+#include "sort.h"
+
+AMAZING_NAMESPACE_BEGIN
+
+template <typename Iter, typename F>
+bool any_of(Iter first, Iter last, F f)
+{
+    for (; first != last; ++first)
+    {
+        if (f(*first))
+            return true;
+    }
+    return false;
+}
+
+template <typename Container, typename F>
+bool any_of(Container const& container, F f)
+{
+    return any_of(begin(container), end(container), f);
+}
+
+template <typename Iter, typename F>
+bool all_of(Iter first, Iter last, F f)
+{
+    for (; first != last; ++first)
+    {
+        if (!f(*first))
+            return false;
+    }
+    return true;
+}
+
+template <typename Container, typename F>
+bool all_of(Container const& container, F f)
+{
+    return all_of(begin(container), end(container), f);
+}
+
+template <typename Iter, typename F>
+void for_each(Iter first, Iter last, F f)
+{
+    for (; first != last; ++first)
+        f(*first);
+}
+
+template <typename Container, typename F>
+void for_each(Container const& container, F f)
+{
+    for_each(begin(container), end(container), f);
+}
+
+AMAZING_NAMESPACE_END
+
+#endif //ITER_H
